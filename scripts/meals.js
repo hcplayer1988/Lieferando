cc56b29ea6bea0bb70c82bb458998d2206090566
Falko <162439151+hcplayer1988@users.noreply.github.com>
2025-06-30 10:20:28 +0200

    add JSON array

---
+++ scripts/meals.js
@@ -0,0 +1,44 @@
+let myDishes = [
+    {
+        "name": "Pizza Margherita",
+        "price": 8.99,
+        "description": "mit Tomatensauce, Mozarella und Basilikum"
+    },
+    {
+        "name": "Pizza Salami",
+        "price": 9.99,
+        "description": "mit Tomatensauce, Salami, Mozzarella und Basilikum"
+    },
+        {
+        "name": "Pizza Funghi",
+        "price": 11.99,
+        "description": "mit Tomatensauce, Kochschinken, Champignons und Mozzarella"
+    },
+    {
+        "name": "Pizza 4 Käse",
+        "price": 12.99,
+        "description": "mit Tomatensauce, Hirtenkäse, Gouda, Cheddar und Mozarella"
+    },
+    {
+        "name": "Pizza Hawaii",
+        "price": 11.99,
+        "description": "mit Tomatensauce, Hinterkochschinken, Ananas und Mozzarella"
+    },
+    {
+        "name": "Pizza Los Angeles",
+        "price": 11.99,
+        "description": "mit Sauce Hollandaise, Broccoli, Hinterkochschinken Mais und Mozzarella"
+    },
+    {
+        "name": "Pizza Döner",
+        "price": 13.99,
+        "description": "mit Tomatensoße, Hähnchenkebab, Tomaten, roten Zwiebeln, Knoblauchsoße, Hirtenkäse und Gouda-Mozzarella-Mix"
+    },
+    {
+        "name": "Pizza Diavolo",
+        "price": 11.99,
+        "description": "mit Tomatensauce, Peperonisalami, Peperoni, roten Zwiebeln Gouda-Mozzarella-Mix"
+    },  
+];
+
+let basket = [];
