2a54ccd3af37645550dd9daee512647634a8a224
Falko <162439151+hcplayer1988@users.noreply.github.com>
2025-07-01 14:23:49 +0200

    update script.js
    basket +counter

--- scripts/script.js
+++ scripts/script.js
@@ -18,11 +18,79 @@ function addToBasket(i) {
     basketPush();
 }
 
+
 function basketPush () {
     basket.push ({
         "name": dish.name,
         "price": dish.price,
         "count": 1
     });
-    
+    document.getElementById('orderPlacedTxt').innerHTML = '';
+    updateBasket();
+    calculatePrice();
 }
+
+function updateBasket() {
+    dokument.getElementById('orderOutput').innerHTML = '';
+    for (let k = 0; k < basket.length; k++) {
+        document.getElementById('orderOutput').innerHTML += getDishes(basket[k], k);
+    }
+}
+
+
+function deleteDishes (i) {
+    basket.splice(i, 1);
+    updateBasket();
+    calculatePrice();
+}
+
+
+function dishesCountMinus(i) {
+    if (basket[i].count === 1) {
+        deleteDishes(i);
+    } else {
+        basket[i].count --;
+    }
+    updateBasket();
+    calculatePrice();
+}
+
+
+function dishesCountPlus () {
+    basket[i].count ++;
+    updateBasket();
+    calculatePrice();
+}
+
+
+calculatePrice() {
+    let totalPrice = 0;
+    for (let l = 0; l < basket.length; l++) {
+        totalPrice += basket[l].count * basket[l].price;
+    }
+    dokument.getElementById()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//placeOrder() {
+
+//}
--- scripts/template.js
+++ scripts/template.js
@@ -6,8 +6,8 @@ function getPizza(pizzen, i) {
         <div class="meal_card">
             <div class="meal">
                 <h2>${pizzen.name}</h2>
-                <div class="add_btn">
-                  <button onclick="addBtn()" id="add-btn">
+                <div class="plus_btn">
+                  <button onclick="addBtn()" id="plusBtn">
                       <img src="./assets/icons/add_icon.png" alt="">
                   </button>
                 </div>
@@ -27,13 +27,13 @@ function getPizza(pizzen, i) {
 
 function getDishes(dish, i) {
     return `
-        <div class="order_output"> 
+        <div class="order_output" id="orderOutput"> 
             <h4 id="">${dish.name}</h4>
             <div class="order_basket_section">
                 <div class="basket_counter">
                     <img onclick="dishesCountMinus(${i})" src="./assets/icons/minus.png" alt="minus">
                     <span id="count">${dish.count}</span>
-                    <img onclick="dishesCountAdd(${i})" src="./assets/icons/plus.png" alt="plus">
+                    <img onclick="dishesCountPlus(${i})" src="./assets/icons/plus.png" alt="plus">
                 </div>
                 <span id="">${(dish.price * dish.count).toFixed(2)}</span>
                 <img onclick="deleteDishes(${i})" src="./assets/icons/trash.png" alt="trash">
